0ef0107a6d6d656500cb28bdc5aa2cc6f8537f61
aliarslan93 <ali.arslan.93@hotmail.com>
2021-11-22 17:59:43 +0300

    Create Nasa Class

---
+++ Nasa.php
@@ -0,0 +1,67 @@
+<?php
+
+class Nasa
+{
+    /**
+     * Connection of Nasa Api
+     * @var string
+     */
+    private $connection_url;
+
+    /**
+     * Date of Earth Date
+     * @var string
+     */
+    private $date;
+
+    /**
+     * API KEY
+     * @var string
+     */
+    private $api_key;
+
+    /**
+     * Nasa Api constructor.
+     */
+    public function __construct()
+    {
+        $this->connection_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=";
+        $this->api_key = "NASA API KEY";
+    }
+
+    /**
+     * @param $date Y-m-d
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        try {
+            $url = $this->connection_url . $this->date;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $array = curl_exec($ch);
+            curl_close($ch);
+            return json_decode($array, true);
+        } catch (\Exception $e) {
+            return json_encode([
+                'status' => 'error',
+                'code' => $e->getCode(),
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+}
